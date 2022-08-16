f1066dc2cdf3fc50b681557be5b1807b845bd5f7
gradel <gerald@hien-online.de>
2022-08-16 02:54:17 +0200

    use path for url function

--- resckeditor/urls.py
+++ resckeditor/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from .views import resources, resource_output
 
 app_name = 'resckeditor'
 urlpatterns = [
-    url(r'^$', resources, name='dialog-data'),
-    url(r'^output/$', resource_output, name='output'),
+    path('', resources, name='dialog-data'),
+    path('output/', resource_output, name='output'),
 ]
